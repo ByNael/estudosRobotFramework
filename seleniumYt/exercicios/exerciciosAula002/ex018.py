295681af6e9535cd825becf74bc37a5c1f8cdcd0
byNael <natannlino@gmail.com>
2024-04-15 19:38:59 -0300

    resolução de ex018 e ex019

--- seleniumYt/exercicios/exerciciosAula002/ex018.py
+++ seleniumYt/exercicios/exerciciosAula002/ex018.py
@@ -4,4 +4,18 @@ e uma posição da mesma faça o quartil dessa posição
 
 p_index = int(p*len(lista))
 """
+def quartil_posicao(lista, p):
+    p_index = int(p * len(lista))
+    
+    sorted_lista = sorted(lista)
+    
+    quartil_value = sorted_lista[p_index]
+    
+    return quartil_value
 
+minha_lista = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+posicao_quartil = 0.99
+
+resultado = quartil_posicao(minha_lista, posicao_quartil)
+
+print(f'O quartil na posição {posicao_quartil} é: {resultado}')
--- seleniumYt/exercicios/exerciciosAula002/ex019.py
+++ seleniumYt/exercicios/exerciciosAula002/ex019.py
@@ -7,3 +7,16 @@ funções embiutidas que podem ajudar
 min(lista) retorna o menor valor
 max(lista) retorna o maior valor
 """
+def dispersao_func(lista):
+    menor_valor=min(lista)
+    maior_valor=max(lista)
+
+    dispersao=maior_valor-menor_valor
+
+    return dispersao
+
+lista = [10,20,30,40,50]
+
+resultado = dispersao_func(lista)
+
+print(f'A dispersão da lista é: {resultado}')
