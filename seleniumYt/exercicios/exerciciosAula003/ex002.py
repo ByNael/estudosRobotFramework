98ef4796ba62d6465f96e43d65138f666efdef52
byNael <natannlino@gmail.com>
2024-04-17 17:47:30 -0300

    resolução do ex002 da aula 003

--- seleniumYt/exercicios/exerciciosAula003/ex002.py
+++ seleniumYt/exercicios/exerciciosAula003/ex002.py
@@ -10,6 +10,7 @@ crie um programa com selenium que
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from time import sleep
+import re
 
 url = 'https://curso-python-selenium.netlify.app/exercicio_02.html'
 
@@ -20,14 +21,23 @@ navegador.get(url)
 sleep(3)
 
 a = navegador.find_element(By.TAG_NAME,'a')
-mensagem_ganhou = navegador.find_elements(By.TAG_NAME, 'p')
 
-while mensagem_ganhou[-1].text != 'você está fazendo algo errado':
+elementos_p = navegador.find_elements(By.TAG_NAME, 'p')
+texto_p = elementos_p[-1].text
+
+numero_desejado = re.search(r'\d+', texto_p).group()
+
+numero_desejado = int(numero_desejado)
+
+while True:
     a.click()
 
+    sleep(1)
+
+    numero_sorteado = re.search(r'\d+', elementos_p[-1].text).group()
+    numero_sorteado = int(numero_sorteado)
+    
+    if numero_sorteado == numero_desejado:
+        break
+
 navegador.quit()
-
-
-
-
-
